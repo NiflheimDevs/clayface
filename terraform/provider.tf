b16867ce2c1b2b369f7e297ea37b7f69b8f80af9
kiarash <kiarashs37@gmail.com>
2026-09-10 01:40:58 +0330

    fix ssh

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,9 @@
 provider "libvirt" {
   alias = "host_a"
-  uri   = "qemu+ssh://${local.lab["hosts"]["host_a"]["user"]}@${local.lab["hosts"]["host_a"]["address"]}/system"
+  uri   = "qemu+ssh://${try(local.lab["hosts"]["host_a"]["user"], local.libvirt_uri_host["user"])}@${try(local.lab["hosts"]["host_a"]["address"], local.libvirt_uri_host["address"])}/system"
 }
 
 provider "libvirt" {
   alias = "host_b"
-  uri   = "qemu+ssh://${local.lab["hosts"]["host_b"]["user"]}@${local.lab["hosts"]["host_b"]["address"]}/system"
+  uri   = "qemu+ssh://${try(local.lab["hosts"]["host_b"]["user"], local.libvirt_uri_host["user"])}@${try(local.lab["hosts"]["host_b"]["address"], local.libvirt_uri_host["address"])}/system"
 }
